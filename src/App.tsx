3be0c0c796bc745aa59406244d86dce0d436f29b
Abaraghi <Abaraghi.Mina@GolrangLeasing.com>
2023-08-13 18:24:34 +0330

    installed and moved folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import WebcamCapture from "./WebcamCapture";
+
 
 function App() {
   return (
     <div className="App">
-      <WebcamCapture />
-      mina
+    <b style={{textAlign:"center",fontSize:'50px',margin:'auto'}}>lets go</b>
     </div>
   );
 }
