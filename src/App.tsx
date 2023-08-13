6b69f2a2866efdc0c782fb972b7d364c6949ba31
Abaraghi <Abaraghi.Mina@GolrangLeasing.com>
2023-08-13 18:16:21 +0330

    added base prj

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import WebcamCapture from "./WebcamCapture";
+
+function App() {
+  return (
+    <div className="App">
+      <WebcamCapture />
+      mina
+    </div>
+  );
+}
+
+export default App;
