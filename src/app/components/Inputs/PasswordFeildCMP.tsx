93da8d227fd126dca6c1e04aa757b145aa3fa8a6
Abaraghi <Abaraghi.Mina@GolrangLeasing.com>
2023-08-19 11:31:33 +0330

    added yup formik and textfields cmp

---
+++ src/app/components/Inputs/PasswordFeildCMP.tsx
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+
+export default function PasswordFieldCMP() {
+  return (
+    <TextField
+      label="Size"
+      id="filled-size-normal"
+      defaultValue="Normal"
+      variant="filled"
+      type="password"
+    />
+  );
+}
---
+++ src/app/components/Inputs/TextFieldCMP.tsx
@@ -0,0 +1,12 @@
+import { TextField } from "@mui/material";
+
+export default function TextFieldCMP() {
+  return (
+    <TextField
+      label="Size"
+      id="filled-size-normal"
+      defaultValue="Normal"
+      variant="filled"
+    />
+  );
+}
